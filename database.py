32563428b7be33a2adfd93aba3057eb160d7db24
Victoriya Roi <victoriyaroi2003@gmail.com>
2021-09-11 15:35:36 +0300

    Add found advert to db

--- database.py
+++ database.py
@@ -3,33 +3,46 @@ import pandas as pd
 
 conn = sqlite3.connect('animals.db', check_same_thread=False)
 
-def create_user():
-    df_users = pd.DataFrame(columns=["Username", "Lat", "Lon"])
-    df_users.to_sql('USER', conn)
-
-def create_lost():
-    df_lost = pd.DataFrame(columns=["Username", "Type", "Sex", "Name", "Features", "Date", "Place", "Photo"])
-    df_lost.to_sql('LOST', conn)
-
-def read(table_name):
+def read(table_name: str):
+    '''
+    Function for testing
+    Print dataFrame
+    '''
     df = pd.read_sql(f'SELECT * FROM {table_name}', conn)
     print(df)
 
-def clear_table(table_name):
+
+def clear_table(table_name: str):
+    '''
+    Function for testing
+    Clear dataFrame
+    '''
     cursor = conn.cursor()
     cursor.execute(f"Delete from {table_name}")
     conn.commit()
 
+
 def is_new_user(username: str) -> bool:
+    '''
+    Return True if user isn't registered yet, and Flase otherwise
+    '''
     df = pd.read_sql(f'SELECT Username FROM USER WHERE Username = "{username}"', conn)
     return df.empty
 
-def add_user(username, lat, lon):
+
+def add_user(username: str, lat: float, lon: float):
+    '''
+    Add new user to database
+    '''
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO USER(Username, Lat, Lon) VALUES (?, ?, ?)', (username, lat, lon))
+    cursor.execute('INSERT INTO USER(Username, Lat, Lon) VALUES (?, ?, ?, ?)', (username, lat, lon, 0))
     conn.commit()
 
+
 def add_lost_advert(username, type, sex, name, features, date, place, photo):
+    '''
+    Add new advertisement about lost animal to database
+    '''
     cursor = conn.cursor()
     cursor.execute(
         '''
@@ -39,8 +52,19 @@ def add_lost_advert(username, type, sex, name, features, date, place, photo):
         (username, type, sex, name, features, date, place, photo))
     conn.commit()
 
+def add_found_advert(username, type, sex, features, date, place, photo):
+    '''
+    Add new advertisement about found animal to database
+    '''
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO FOUND(Username, Type, Sex, Features, Date, Place, Photo)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (username, type, sex, features, date, place, photo))
+    conn.commit()
 
-# create_lost()
 # clear_table("LOST")
-add_lost_advert("test", "cat", "F", "Levchik", "Blue eyes", "24/04/21", "Lviv", "photo.jpg")
-read("LOST")
+# add_found_advert("test", "cat", "F", "Blue eyes", "24/04/21", "Lviv", "photo.jpg")
+# read("FOUND")
